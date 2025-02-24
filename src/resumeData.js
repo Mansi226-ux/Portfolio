79767f1c7c7fbc6c1d59f2f1b6a900201b642a5b
Mansi226-ux <ks2876265@gmail.com>
2025-02-24 22:36:43 +0530

    PORTFOLIO

---
+++ src/resumeData.js
@@ -0,0 +1,119 @@
+let resumeData = {
+  //"imagebaseurl":"https://rbhatia46.github.io/",
+  "name": "Mansi Sahu",
+  "role": "Frontend Developer",
+  "linkedinId": "https://www.linkedin.com/in/mansi-sahu-1354b630a?",
+  "skypeid": "https://www.facebook.com/profile.php?id=61558975799201",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/mansi-sahu-1354b630a?",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "intagram",
+      "url": "https://www.instagram.com/invites/contact/?igsh=oo5rrhm3th&utm_ content=ng22pff",
+      "className": "fa fa-instagram"
+    },
+    {
+      "name": "skype",
+      "url": "https://www.facebook.com/profile.php?id=61558975799201",
+      "className": "fa fa-facebook"
+    }
+  ],
+  "aboutme": "I am currently a pre-final year student at The DAVV University and pursuing my MSC from here. I am a self taught frontend Web Developer, currently diving deeper into Backend  Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  "address": "India",
+  "website": "https://msahu6.github.io",
+  "education": [
+    {
+      "UniversityName": "Davi Ahilya Vishv vidhyalay",
+      "MonthOfPassing": "july",
+      "YearOfPassing": "2024",
+      "Achievements": " Awarded the university’s Distinguished Research Grant for innovative projects on renewable energy resources."
+    }
+    /*
+    {
+      "UniversityName":"Some University",
+      "specialization":"Some specialization",
+      "MonthOfPassing":"July",
+      "YearOfPassing":"2024",
+      "Achievements":"Some Achievements"
+    }*/
+  ],
+  "work": [
+    {
+      "CompanyName": "YPSILON IT SOLUTION",
+      "specialization": "Designed and tested UI Component for web applications, utilizing HTML, CSS, JavaScript , BootStrap and React JS  to enhance user experience.",
+      "MonthOfLeaving": "Nov",
+      "YearOfLeaving": "2024",
+      "Achievements": "Developed REST fullservices using Node.js and Express, integrating JSON Web Token for secure authorization mechanisms.  *Designed and tested UI Component for web applications, utilizing HTML, CSS, JavaScript, BootStrap and React JS  to enhance user experience.  *Validate input before submitting to the backend.  *Translated user and business needs into functional front - end design using Javascript, HTML, and CSS.  *Worked on the development of user interface for Stealth(identity) from scratch."
+    }
+      /*
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }*/
+    ],
+"skillsDescription": "My skills here",
+  "skills": [
+    {
+      "skillname": "HTML"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Java Script"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "Nodejs"
+    },
+    {
+      "skillname": "express"
+    },
+    {
+      "skillname": "mongoodb"
+    }
+  ],
+    "portfolio": [
+      {
+        "name": "Ecommerce Website",
+        "description": "	Designed a web application that simplifies the online shopping experience, making it easier for customers to buy latest clothing products. Implemented firebase authentication for login and register. Built a profile visualizer website using ReactJS.",
+        "imgurl": "images/portfolio/ecommerceimg.avif"
+      },
+      {
+        "name": "project2",
+        "description": "carvila",
+        "imgurl": "images/portfolio/farmerboy.jpg"
+      },
+      {
+        "name": "project3",
+        "description": "todo list",
+        "imgurl": "images/portfolio/judah.jpg"
+      },
+      {
+        "name": "project4",
+        "description": "portfolio",
+        "imgurl": "images/portfolio/into-the-light.jpg"
+      }
+    ],
+      "testimonials": [
+        {
+          "description": "This is a sample testimonial",
+          "name": "Some technical guy"
+        },
+        {
+          "description": "This is a sample testimonial",
+          "name": "Some technical guy"
+        }
+      ]
+  }
+
+export default resumeData
